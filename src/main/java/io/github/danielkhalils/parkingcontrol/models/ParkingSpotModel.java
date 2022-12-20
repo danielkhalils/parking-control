719e966741e2cf8c946b9ed75a7e82cefdd5d24f
danielkhalils <dkses.hef@uea.edu.br>
2022-12-19 22:29:27 -0400

    Inicializando projeto

---
+++ src/main/java/io/github/danielkhalils/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,33 @@
+package io.github.danielkhalils.parkingcontrol.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "RB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final long serializableUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+
+}
